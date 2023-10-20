251759758cbd391d2db6fcd97bbb340b0150ff85
gtamaral <rodrigoamaral003@gmail.com>
2023-10-20 16:48:24 -0300

    validando form

--- src/pages/jogo/[portas]/[temPresente].tsx
+++ src/pages/jogo/[portas]/[temPresente].tsx
@@ -10,9 +10,22 @@ import { useRouter } from "next/router";
 
 export default function jogo() {
     const router = useRouter()
+    // usst
+    
+    const [valido, setValido] = useState(false)
     const [portas, setPortas] = useState([])
 
     console.log(router?.query)
+
+    useEffect(() => {
+        const portas = +router.query.portas
+        const temPresente = +router.query.temPresente
+
+        const qntPortasValidas = portas >= 3 && portas <= 100
+        const temPresenteValido = temPresente >= 1 && temPresente <= portas
+
+        setValido(qntPortasValidas && temPresenteValido)
+    }, [portas])
 
     useEffect(() => {
         const portas = +router.query.portas
@@ -27,7 +40,7 @@ export default function jogo() {
 
   // console.log(criarPortas(3,2))
     function renderizarPortas() {
-        return portas.map(porta => {
+        return valido && portas.map(porta => {
             return <Porta key={porta.numero} value={porta} onChange={novaPorta => setPortas(ataualizarPortas(portas, novaPorta))} />
         })
     }
@@ -35,7 +48,10 @@ export default function jogo() {
     return (
         <div id={styles.jogo}>
             <div className={styles.portas}>
-                {renderizarPortas()}
+                {valido ?
+                renderizarPortas() :
+                <h2>Valores invalidos!</h2>
+            }
             </div>
             <div className={styles.botoes}>
                 <Link href="/">
