9824a9cad523884b09c4236028a6324820a545ea
gtamaral <rodrigoamaral003@gmail.com>
2023-10-20 12:35:26 -0300

    add form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,29 @@ import { useState } from "react";
 import Porta from "../../components/Porta";
 import PortaModel from "../../model/porta";
 import { ataualizarPortas, criarPortas } from "../../functions/portas";
+import Card from "../../components/Card";
+import styles from "../styles/Formulario.module.css"
+import Link from "next/link";
 
-export default function Home() {
 
-  
+export default function Formulario() {
 
   return (
-    <div style={{display: "flex"}}>
-      <h1>Inicio do jogo</h1>
+    <div className={styles.formulario}>
+      <div>
+        <Card bgcolor="#c0392c">
+          <h1>Reward door</h1>
+        </Card>
+        <Card></Card>
+      </div>
+      <div>
+        <Card></Card>
+        <Card bgcolor="#28a085">
+          <Link href={`/jogo/4/2`}>
+            <h2 className={styles.link}>iniciar</h2>
+          </Link>
+        </Card>
+      </div>
     </div>
   )
 }
